d1e9fc1eb78545badd2a314ad1d24bfb22a9bfe0
toyoshima <sxtsham9129@yahoo.ne.jp>
2022-09-22 19:47:50 +0900

    x-guest-layout削除、composer.json変更

--- resources/views/contact/thanks.blade.php
+++ resources/views/contact/thanks.blade.php
@@ -1,4 +1,15 @@
-<x-guest-layout>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- css読み込み -->
+    @vite('resources/css/app.css')
+</head>
+<body>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -41,4 +52,5 @@
             </div>
         </div>
     </div>
-</x-guest-layout>
+</body>
+</html>
